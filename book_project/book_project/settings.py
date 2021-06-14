9363b90091f0e25b1e41424e2d5638b9ab24b37e
divitrao <divitrao97d@gmail.com>
2021-06-14 20:19:13 +0530

    added crispy forms

--- book_project/book_project/settings.py
+++ book_project/book_project/settings.py
@@ -40,8 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 # Local
     'users.apps.UsersConfig', 
-    'pages.apps.PagesConfig'
+    'pages.apps.PagesConfig',
+
+#third party
+    'crispy_forms'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
@@ -124,7 +129,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_FINDERS = [
+"django.contrib.staticfiles.finders.FileSystemFinder",
+"django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
